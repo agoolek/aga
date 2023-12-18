e0c62e3696625c48d7722279540d77fb62df82e3
Agata <agata.janus@edu.uekat.pl>
2023-12-18 11:58:33 +0100

    files

--- zad_6_.py
+++ zad_6_.py
@@ -5,12 +5,13 @@ Created on Mon Nov  6 12:48:23 2023
 @author: student
 """
 
+
 def przekształcenie(lista1, lista2):
 
     połączona_lista = list(set(lista1 + lista2))
     wynikowa_lista = [x**3 for x in połączona_lista]
-    
     return wynikowa_lista
+
 
 lista1 = [1, 2, 3, 4]
 lista2 = [3, 4, 5, 6]
