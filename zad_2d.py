650e5d6d0064b6bf42dca7c1fc0652a76f6a7b42
Agata <agata.janus@edu.uekat.pl>
2023-12-18 12:06:47 +0100

    files

--- zad_2d.py
+++ zad_2d.py
@@ -5,7 +5,7 @@ Created on Mon Oct 16 12:27:19 2023
 @author: student
 """
 import random
-l=[random.randint(0,100) for x in range(10)]
+list = [random.randint(0, 100) for x in range(10)]
 print(l)
-co_druga = l[::2]
+co_druga = list[::2]
 print(co_druga)
